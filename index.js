71f6c6a37b2b887f397ea914bbbbac3ec7454a3f
Shivam Rawat <shivambuddy222@gmail.com>
2025-02-23 20:42:22 -0800

    added environment variables

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const express = require('express')
 const app = express();
-const port = 200;
-const {connectToMongoDb} = require("./Database/Connect")
+const { connectToMongoDb } = require("./Database/Connect")
 const Urlmongo = require('./Models/Urls')
-
+const env = require("dotenv").config();
 app.set('view engine', 'ejs');
-
-connectToMongoDb('mongodb+srv://rawat:rawat@rawatprojects.q0uwa.mongodb.net/?retryWrites=true&w=majority&appName=RawatProjects');
+const port = process.env.PORT | 2002;
+connectToMongoDb(process.env.MONGO_URL);
 
 const urlrouter = require('./Routers/urlrouter')
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('Home')
 })
-app.use("/api",urlrouter)
+app.use("/api", urlrouter)
 
-app.get('/:id',async(req,res)=>{
+app.get('/:id', async (req, res) => {
     let shortid = req.params.id;
-    let redirect ;
-    let data = await Urlmongo.findOne({shortid})
-    if(data)
-    {
+    let redirect;
+    let data = await Urlmongo.findOne({ shortid })
+    if (data) {
         redirect = data.ActualUrl;
         res.redirect(redirect);
     }
-    else{
+    else {
         res.send("response is null")
     }
 })
 
-app.listen(port,()=>{
-    console.log( `http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
 })
